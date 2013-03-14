94829fa3bcdaae174c52c098409bd0b3bba287c3
thiago <thiagosoara@gmail.com>
2013-03-14 10:14:55 -0300

    Criação do código da parte de disciplinas.

--- code/src/br/com/redu/redumobile/activities/LectureActivity.java
+++ code/src/br/com/redu/redumobile/activities/LectureActivity.java
@@ -1,7 +1,18 @@
 package br.com.redu.redumobile.activities;
 
+import br.com.redu.redumobile.R;
 import android.app.Activity;
+import android.os.Bundle;
 
 public class LectureActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_lecture);
+		
+	}
 
 }
