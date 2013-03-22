5f0f273e209e57bb6595f45280886c2dcd04a71c
Luis Medeiros <lpmfilho@gmail.com>
2013-03-22 15:31:55 -0300

    Fixes notification refreshing logic

--- code/src/br/com/redu/redumobile/db/DbHelper.java
+++ code/src/br/com/redu/redumobile/db/DbHelper.java
@@ -29,30 +29,36 @@ public class DbHelper extends SQLiteOpenHelper {
 	public static final String COLUMN_LECTURE_ALREADY_SEEN = "lecture_already_seen";
 	public static final String COLUMN_LAST_SEEN = "last_seen";
 
-	private SQLiteDatabase db;  
-
 	// Database creation sql statement
 	private static final String CREATE_TABLE_STATUS = "CREATE TABLE "
 			+ TABLE_STATUS + "(" 
 			+ COLUMN_ID + " TEXT PRIMARY KEY , " 
 			+ COLUMN_TYPE + " TEXT NOT NULL, " 
-			+ COLUMN_LOGEABLE_TYPE + " TEXT NOT NULL, " 
-			+ COLUMN_CREATE_AT_IN_MILLIS + " INTEGER NOT NULL, "
+			+ COLUMN_LOGEABLE_TYPE + " TEXT, " 
+			+ COLUMN_CREATE_AT_IN_MILLIS + " INTEGER, "
 			+ COLUMN_LECTURE_ALREADY_SEEN + " TEXT NOT NULL, "
-			+ COLUMN_LAST_SEEN + " INTEGER NOT NULL, "
-			+ COLUMN_TEXT + " INTEGER NOT NULL);";
+			+ COLUMN_LAST_SEEN + " INTEGER, "
+			+ COLUMN_TEXT + " TEXT NOT NULL);";
 	
-	// TODO
-//	private static final String CREATE_TABLE_USER = "CREATE TABLE "
-//			+ TABLE_USER + "(" 
-//			+ COLUMN_ID + " integer primary key autoincrement, " 
-//			+ COLUMN_LOGIN + "login not null);";
-
-	public DbHelper(Context context) {
+	private static DbHelper instance;
+	
+	private DbHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        this.db = this.getWritableDatabase();  
     }  
 
+	public static DbHelper newInstance(Context context) {
+		instance = new DbHelper(context);
+		return instance;
+	}
+
+	public static DbHelper getInstance(Context context) {
+		if(instance == null) {
+			instance = new DbHelper(context);
+		}
+		
+		return instance;
+	}
+	
 	@Override
 	public void onCreate(SQLiteDatabase database) {
 		database.execSQL(CREATE_TABLE_STATUS);
@@ -60,54 +66,106 @@ public class DbHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		Log.w(DbHelper.class.getName(),
-				"Upgrading database from version " + oldVersion + " to "
-						+ newVersion + ", which will destroy all old data");
+		Log.w(DbHelper.class.getName(), 
+				"Upgrading database from version " + oldVersion + " to " + newVersion + ", which will destroy all old data");
 		db.execSQL("DROP TABLE IF EXISTS " + TABLE_STATUS);
 		onCreate(db);
 	}
 	
-	public List<Status> getStatus(long olderThan, int count) {
+	synchronized public List<Status> getStatus(long olderThan, int count) {
 		List<Status> statuses = new ArrayList<Status>(count);  
+
+		SQLiteDatabase db = this.getReadableDatabase();  
         Cursor cursor;  
   
-        cursor = this.db.query(TABLE_STATUS, null, 
+        cursor = db.query(TABLE_STATUS, null, 
         		COLUMN_CREATE_AT_IN_MILLIS + " < ?", 
         		new String[] {String.valueOf(olderThan)}, 
         		null, null, 
         		COLUMN_CREATE_AT_IN_MILLIS + " DESC", 
         		String.valueOf(count));  
   
-        if (cursor.moveToFirst()) {  
-        	while(cursor.moveToNext()) {
-        		Status status = new Status();
-        		status.id = cursor.getString(cursor.getColumnIndex(COLUMN_ID));
-        		status.type = cursor.getString(cursor.getColumnIndex(COLUMN_TYPE));
-        		status.logeable_type = cursor.getString(cursor.getColumnIndex(COLUMN_LOGEABLE_TYPE));
-        		status.text = cursor.getString(cursor.getColumnIndex(COLUMN_TEXT));
-        		status.created_at_in_millis = cursor.getLong(cursor.getColumnIndex(COLUMN_CREATE_AT_IN_MILLIS));
-        		
-        		int lectureAreadySeen = cursor.getInt(cursor.getColumnIndex(COLUMN_TEXT));
-    			status.lectureAreadySeen = (lectureAreadySeen == 0) ? false : true;
-        		
-        		int lastSeen = cursor.getInt(cursor.getColumnIndex(COLUMN_TEXT));
-    			status.lastSeen = (lastSeen == 0) ? false : true;
-        		
-        		statuses.add(status);
-        	}
-        }
+    	while(cursor.moveToNext()) {
+			statuses.add(getCurrentStatusInCursor(cursor));
+    	}
         
         cursor.close();  
+        db.close();
   
         return statuses;  
 	}
 	
-	public long putStatus(Status status) {
+	synchronized public List<Status> getNewLecturesStatus(long olderThan, int count) {
+		List<Status> statuses = new ArrayList<Status>(count);  
+		
+		SQLiteDatabase db = this.getReadableDatabase();  
+		Cursor cursor;  
+		
+		cursor = db.query(TABLE_STATUS, null, 
+				COLUMN_CREATE_AT_IN_MILLIS + " < ? AND " + COLUMN_LOGEABLE_TYPE + " = ?", 
+				new String[] {String.valueOf(olderThan), Status.LOGEABLE_TYPE_LECTURE}, 
+				null, null, 
+				COLUMN_CREATE_AT_IN_MILLIS + " DESC", 
+				String.valueOf(count));  
+		
+		while(cursor.moveToNext()) {
+			statuses.add(getCurrentStatusInCursor(cursor));
+		}
+		
+		cursor.close();  
+		db.close();
+		
+		return statuses;  
+	}
+	
+	synchronized public List<Status> getLastSeenStatus(long olderThan, int count) {
+		List<Status> statuses = new ArrayList<Status>(count);  
+		
+		SQLiteDatabase db = this.getReadableDatabase();  
+		Cursor cursor;  
+		
+		cursor = db.query(TABLE_STATUS, null, 
+				COLUMN_CREATE_AT_IN_MILLIS + " < ? AND " + COLUMN_LAST_SEEN + " = ?", 
+				new String[] {String.valueOf(olderThan), "1"}, 
+				null, null, 
+				COLUMN_CREATE_AT_IN_MILLIS + " DESC", 
+				String.valueOf(count));  
+		
+		while(cursor.moveToNext()) {
+			statuses.add(getCurrentStatusInCursor(cursor));
+		}
+		
+		cursor.close();  
+		db.close();
+		
+		return statuses;  
+	}
+	
+	private Status getCurrentStatusInCursor(Cursor cursor) {
+		Status status = new Status();
+		status.id = cursor.getString(cursor.getColumnIndex(COLUMN_ID));
+		status.type = cursor.getString(cursor.getColumnIndex(COLUMN_TYPE));
+		status.logeable_type = cursor.getString(cursor.getColumnIndex(COLUMN_LOGEABLE_TYPE));
+		status.text = cursor.getString(cursor.getColumnIndex(COLUMN_TEXT));
+		status.created_at_in_millis = cursor.getLong(cursor.getColumnIndex(COLUMN_CREATE_AT_IN_MILLIS));
+		
+		int lectureAreadySeen = cursor.getInt(cursor.getColumnIndex(COLUMN_TEXT));
+		status.lectureAreadySeen = (lectureAreadySeen == 0) ? false : true;
+		
+		int lastSeen = cursor.getInt(cursor.getColumnIndex(COLUMN_TEXT));
+		status.lastSeen = (lastSeen == 0) ? false : true;
+		
+		return status;
+	}
+	
+	synchronized public long putStatus(Status status) {
+		SQLiteDatabase db = this.getWritableDatabase();  
+
         ContentValues values = new ContentValues();
         values.put(COLUMN_TEXT, status.text);
         values.put(COLUMN_ID, status.id);
-        values.put(COLUMN_TYPE, status.logeable_type);
-        values.put(COLUMN_LOGEABLE_TYPE, status.type);
+        values.put(COLUMN_TYPE, status.type);
+        values.put(COLUMN_LOGEABLE_TYPE, status.logeable_type);
         
         if(status.created_at_in_millis == 0) {
         	try {
@@ -121,7 +179,11 @@ public class DbHelper extends SQLiteOpenHelper {
         values.put(COLUMN_LECTURE_ALREADY_SEEN, status.lectureAreadySeen);
         values.put(COLUMN_LAST_SEEN , status.lastSeen);
         
-        return this.db.insert(TABLE_STATUS, null, values);
+        long id = db.insert(TABLE_STATUS, null, values);
+        
+        db.close();
+        
+        return id;
 	}
 	
 	//TODO 
@@ -129,8 +191,10 @@ public class DbHelper extends SQLiteOpenHelper {
 	 * Get the millis of the most recent Status saved at the db
 	 * @return the creation time, in millis, of the most recent Status saved at the db. 
 	 */
-	public long getTimestamp() {
+	synchronized public long getTimestamp() {
 		long timestamp = 0l;
+		
+		SQLiteDatabase db = this.getReadableDatabase();  
         Cursor cursor;  
         
         String query = new StringBuffer("SELECT MAX(").
@@ -139,15 +203,15 @@ public class DbHelper extends SQLiteOpenHelper {
         		append(TABLE_STATUS).
         		toString();
         
-        cursor = this.db.rawQuery(query, null);
+        cursor = db.rawQuery(query, null);
   
         if (cursor.moveToFirst()) {  
     		timestamp = cursor.getLong(0);
         }
         
         cursor.close();  
-  
+        db.close();
+        
         return timestamp;
 	}
-
 }
--- code/src/br/com/redu/redumobile/util/SettingsHelper.java
+++ code/src/br/com/redu/redumobile/util/SettingsHelper.java
@@ -3,11 +3,18 @@ package br.com.redu.redumobile.util;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import br.com.redu.redumobile.R;
 
 public class SettingsHelper {
 
-	static public boolean get(Context ctx, String key) {
+	public static final int KEY_ACTIVATED_NOTIFICATIONS = R.string.activate_notifications_key;
+	public static final int KEY_WHEN_ANSWER_ME = R.string.when_answer_me_key;
+	public static final int KEY_NEW_LECTURES = R.string.new_lectures_key;
+	public static final int KEY_NEW_SUBJECTS = R.string.new_subject_key;
+	public static final int KEY_NEW_COURSES = R.string.new_courses_key;
+
+	static public boolean get(Context ctx, int key) {
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
-		return prefs.getBoolean(key, true);
+		return prefs.getBoolean(ctx.getString(key), true);
 	}
 }
