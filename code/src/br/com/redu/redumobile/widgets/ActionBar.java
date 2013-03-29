e524b10c20ba97144d9dffe5111ca10622ab377e
Luis Medeiros <lpmfilho@gmail.com>
2013-03-28 23:27:12 -0300

    Changing action bar home button

--- code/src/br/com/redu/redumobile/widgets/ActionBar.java
+++ code/src/br/com/redu/redumobile/widgets/ActionBar.java
@@ -38,8 +38,7 @@ public class ActionBar extends FrameLayout {
 		tvTitle.setText(title);
 
 		ImageButton ibUp = (ImageButton) mContent.findViewById(R.id.b_up);
-		///TODO this above
-		//		ibUp.setImageResource(showCaret ? R.drawable.action_bar_home_caret : R.drawable.action_bar_home);
+		ibUp.setImageResource(showCaret ? R.drawable.action_bar_home_caret : R.drawable.action_bar_home);
 
 		addView(mContent, new LayoutParams(LayoutParams.MATCH_PARENT,
 				LayoutParams.MATCH_PARENT));
