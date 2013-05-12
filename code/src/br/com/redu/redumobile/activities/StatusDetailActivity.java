e77d59edbea6e88a0b83e3cba09e2ef16ef3a3c7
Luis Medeiros <lpmfilho@gmail.com>
2013-05-12 10:26:46 -0300

    Fixing empty message answers status detail

--- code/src/br/com/redu/redumobile/activities/StatusDetailActivity.java
+++ code/src/br/com/redu/redumobile/activities/StatusDetailActivity.java
@@ -27,15 +27,14 @@ import br.com.redu.redumobile.widgets.StatusComposer.OnStatusComposerListener;
 public class StatusDetailActivity extends BaseActivity {
 
 	public static final String EXTRAS_STATUS = "EXTRAS_STATUS";
-	
+
 	private LayoutInflater mInflater;
-	
+
 	private ListView mListView;
-	private TextView mTvEmptyList;
-	
+
 	private StatusDetailAdapter mAdapter;
 	private View mFooterLoadingAnswers;
-	
+
 	private Status mStatus;
 
 	@Override
@@ -47,67 +46,79 @@ public class StatusDetailActivity extends BaseActivity {
 
 		setActionBarTitle(mStatus.getLastBreadcrumb());
 
-		if(!mStatus.isPostedOnUserWall()) {
-			addActionToActionBar(R.drawable.bt_go_to_wall, new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					Intent i = null;
-					if(mStatus.isPostedOnLectureWall()) {
-						i = new Intent(StatusDetailActivity.this, LectureActivity.class);
-						i.putExtra(LectureActivity.EXTRAS_SUBJECT_ID, mStatus.getSubjectId());
-						i.putExtra(LectureActivity.EXTRAS_LECTURE_ID, mStatus.getLectureId());
-						
-					} else if(mStatus.isPostedOnSpaceWall()) {
-						i = new Intent(StatusDetailActivity.this, SpaceActivity.class);
-						i.putExtra(SpaceActivity.EXTRAS_SPACE_ID, mStatus.getSpaceId());
+		if (!mStatus.isPostedOnUserWall()) {
+			addActionToActionBar(R.drawable.bt_go_to_wall,
+					new OnClickListener() {
+						@Override
+						public void onClick(View v) {
+							Intent i = null;
+							if (mStatus.isPostedOnLectureWall()) {
+								i = new Intent(StatusDetailActivity.this,
+										LectureActivity.class);
+								i.putExtra(LectureActivity.EXTRAS_SUBJECT_ID,
+										mStatus.getSubjectId());
+								i.putExtra(LectureActivity.EXTRAS_LECTURE_ID,
+										mStatus.getLectureId());
 
-						Bundle extras = new Bundle();
-						extras.putString(SpaceActivity.EXTRAS_ENVIRONMENT_PATH, mStatus.getEnvironmentPath());
-						extras.putString(SpaceActivity.EXTRAS_SPACE_ID, mStatus.getSpaceId());
-						setUpClass(SpaceActivity.class, extras);
-					
-					} else if(mStatus.isPostedOnUserWall()) {
-						setUpClass(HomeActivity.class);
-					}
-					
-					startActivity(i);
-				}
-			});
+							} else if (mStatus.isPostedOnSpaceWall()) {
+								i = new Intent(StatusDetailActivity.this,
+										SpaceActivity.class);
+								i.putExtra(SpaceActivity.EXTRAS_SPACE_ID,
+										mStatus.getSpaceId());
+
+								Bundle extras = new Bundle();
+								extras.putString(
+										SpaceActivity.EXTRAS_ENVIRONMENT_PATH,
+										mStatus.getEnvironmentPath());
+								extras.putString(SpaceActivity.EXTRAS_SPACE_ID,
+										mStatus.getSpaceId());
+								setUpClass(SpaceActivity.class, extras);
+
+							} else if (mStatus.isPostedOnUserWall()) {
+								setUpClass(HomeActivity.class);
+							}
+
+							startActivity(i);
+						}
+					});
 		}
-		
+
 		mInflater = LayoutInflater.from(this);
-		
-		mFooterLoadingAnswers = mInflater.inflate(R.layout.status_detail_footer_loading_answers, null);
-		
-		mTvEmptyList = (TextView) findViewById(R.id.tv_empty_list);
-		mTvEmptyList.setText("Não há respostas, seja o primeiro a responder");
-		
+
+		mFooterLoadingAnswers = mInflater.inflate(
+				R.layout.status_detail_footer_loading_answers, null);
+
 		mListView = (ListView) findViewById(R.id.list);
 		mListView.addHeaderView(createStatusHeaderView(mStatus));
-	
-		((StatusComposer) findViewById(R.id.status_composer)).setOnStatusComposerListener(new OnStatusComposerListener() {
-			@Override
-			public void onSendClicked(String text) {
-				new PostAnswerTask().execute(mStatus.id, text);
-			}
-		});
-		
+
+		((StatusComposer) findViewById(R.id.status_composer))
+				.setOnStatusComposerListener(new OnStatusComposerListener() {
+					@Override
+					public void onSendClicked(String text) {
+						new PostAnswerTask().execute(mStatus.id, text);
+					}
+				});
+
 		mAdapter = new StatusDetailAdapter(getApplicationContext(), null);
 		mListView.setAdapter(mAdapter);
 
 		new LoadAnswersStatus(mStatus.id).execute();
-  	}
-	
+	}
+
 	private View createStatusHeaderView(Status status) {
-		View v = mInflater.inflate(R.layout.status_detail_header_original_status, null);
+		View v = mInflater.inflate(
+				R.layout.status_detail_header_original_status, null);
 
 		((Breadcrumb) v.findViewById(R.id.tv_breadcrumb)).setStatus(status);
 
-		((WebCachedImageView) v.findViewById(R.id.iv_photo)).setImageUrl(status.user.getThumbnailUrl());
-		((TextView) v.findViewById(R.id.tv_date)).setText(DateUtil.getFormattedStatusCreatedAt(status));
+		((WebCachedImageView) v.findViewById(R.id.iv_photo))
+				.setImageUrl(status.user.getThumbnailUrl());
+		((TextView) v.findViewById(R.id.tv_date)).setText(DateUtil
+				.getFormattedStatusCreatedAt(status));
 
-		StringBuffer userActionBuffer = new StringBuffer().append("<b>").append(status.user.getCompleteName()).append("</b>");
-		
+		StringBuffer userActionBuffer = new StringBuffer().append("<b>")
+				.append(status.user.getCompleteName()).append("</b>");
+
 		if (status.isActivityType()) {
 			userActionBuffer.append(" comentou");
 			v.findViewById(R.id.iv_help_icon).setVisibility(View.GONE);
@@ -121,34 +132,38 @@ public class StatusDetailActivity extends BaseActivity {
 			v.findViewById(R.id.iv_help_icon).setVisibility(View.VISIBLE);
 		}
 
-		((TextView) v.findViewById(R.id.tv_user_action)).setText(Html.fromHtml(userActionBuffer.toString()));
+		((TextView) v.findViewById(R.id.tv_user_action)).setText(Html
+				.fromHtml(userActionBuffer.toString()));
 
 		((TextView) v.findViewById(R.id.tv_text)).setText(status.text);
 
 		return v;
 	}
-	
-//	private View createLocalOfPublicationHeaderView(Status status) {
-//		View v = mInflater.inflate(R.layout.status_detail_header_publishing_local, null);
-//		
-//		Statusable statusable = status.getStatusable();
-//		
-//		if(statusable.isTypeUser()) {
-//			v = null;
-//		
-//		} else if(statusable.isTypeLecture()) {
-//			TextView tv = ((TextView) v.findViewById(R.id.tv_title));
-//			tv.setText(statusable.name);
-//			tv.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_aula_azul, 0, 0, 0);
-//			
-//		} else if(statusable.isTypeSpace()) {
-//			TextView tv = ((TextView) v.findViewById(R.id.tv_title));
-//			tv.setText(statusable.name);
-//			tv.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_disciplina_azul, 0, 0, 0);
-//		}
-//		
-//		return v;
-//	}
+
+	// private View createLocalOfPublicationHeaderView(Status status) {
+	// View v =
+	// mInflater.inflate(R.layout.status_detail_header_publishing_local, null);
+	//
+	// Statusable statusable = status.getStatusable();
+	//
+	// if(statusable.isTypeUser()) {
+	// v = null;
+	//
+	// } else if(statusable.isTypeLecture()) {
+	// TextView tv = ((TextView) v.findViewById(R.id.tv_title));
+	// tv.setText(statusable.name);
+	// tv.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_aula_azul, 0, 0,
+	// 0);
+	//
+	// } else if(statusable.isTypeSpace()) {
+	// TextView tv = ((TextView) v.findViewById(R.id.tv_title));
+	// tv.setText(statusable.name);
+	// tv.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_disciplina_azul,
+	// 0, 0, 0);
+	// }
+	//
+	// return v;
+	// }
 
 	class LoadAnswersStatus extends
 			AsyncTask<Void, Void, List<br.com.developer.redu.models.Status>> {
@@ -157,7 +172,7 @@ public class StatusDetailActivity extends BaseActivity {
 		public LoadAnswersStatus(String statusId) {
 			mStatusId = statusId;
 		}
-		
+
 		@Override
 		protected void onPreExecute() {
 			super.onPreExecute();
@@ -165,14 +180,16 @@ public class StatusDetailActivity extends BaseActivity {
 		}
 
 		@Override
-		protected List<br.com.developer.redu.models.Status> doInBackground(Void... params) {
+		protected List<br.com.developer.redu.models.Status> doInBackground(
+				Void... params) {
 			List<br.com.developer.redu.models.Status> answers;
 
 			try {
-				DefaultReduClient redu = ReduApplication.getReduClient(StatusDetailActivity.this);
+				DefaultReduClient redu = ReduApplication
+						.getReduClient(StatusDetailActivity.this);
 				answers = redu.getAnswers(mStatusId);
 			} catch (Exception e) {
-				//TODO show message for internet problems
+				// TODO show message for internet problems
 				e.printStackTrace();
 				answers = null;
 			}
@@ -181,56 +198,69 @@ public class StatusDetailActivity extends BaseActivity {
 		}
 
 		@Override
-		protected void onPostExecute(List<br.com.developer.redu.models.Status> answers) {
+		protected void onPostExecute(
+				List<br.com.developer.redu.models.Status> answers) {
 			if (answers != null) {
 				if (answers.size() > 0) {
-					DbHelper dbHelper = DbHelper.getInstance(StatusDetailActivity.this);
+					DbHelper dbHelper = DbHelper
+							.getInstance(StatusDetailActivity.this);
 					dbHelper.updateStatusAnswersCount(mStatusId, answers.size());
-						
+
 					mAdapter.addAll(answers);
 					mAdapter.notifyDataSetChanged();
+
+					mListView.removeFooterView(mFooterLoadingAnswers);
 				} else {
-					mTvEmptyList.setVisibility(View.VISIBLE);
+					mFooterLoadingAnswers.findViewById(R.id.pb).setVisibility(View.GONE);
+					((TextView) mFooterLoadingAnswers
+							.findViewById(R.id.tv_text))
+							.setText("Não há respostas, seja o primeiro a responder");
 				}
 			} else {
 				// TODO handler no internet problem connection
 			}
-			
-			mListView.removeFooterView(mFooterLoadingAnswers);
+
 		}
 	}
-	
-	class PostAnswerTask extends AsyncTask<String, Void, br.com.developer.redu.models.Status> {
+
+	class PostAnswerTask extends
+			AsyncTask<String, Void, br.com.developer.redu.models.Status> {
 
 		@Override
 		protected void onPreExecute() {
 			showProgressDialog("Enviando sua resposta…");
 		}
-		
+
 		@Override
-		protected br.com.developer.redu.models.Status doInBackground(String... params) {
+		protected br.com.developer.redu.models.Status doInBackground(
+				String... params) {
 			String statusId = params[0];
 			String text = params[1];
-			
+
 			br.com.developer.redu.models.Status status;
 			try {
-				DefaultReduClient redu = ReduApplication.getReduClient(StatusDetailActivity.this);
+				DefaultReduClient redu = ReduApplication
+						.getReduClient(StatusDetailActivity.this);
 				status = redu.postAnswer(statusId, text);
-			} catch(Exception e) {
+			} catch (Exception e) {
 				status = null;
 			}
-			
+
 			return status;
 		}
-		
+
 		@Override
 		protected void onPostExecute(br.com.developer.redu.models.Status result) {
 			dismissProgressDialog();
-			
-			if(result == null) {
-				Toast.makeText(StatusDetailActivity.this, "Não foi possível enviar sua resposta.", Toast.LENGTH_SHORT).show();
+
+			if (result == null) {
+				Toast.makeText(StatusDetailActivity.this,
+						"Não foi possível enviar sua resposta.",
+						Toast.LENGTH_SHORT).show();
 			} else {
-				Toast.makeText(StatusDetailActivity.this, "Resposta enviada com sucesso.", Toast.LENGTH_SHORT).show();
+				Toast.makeText(StatusDetailActivity.this,
+						"Resposta enviada com sucesso.", Toast.LENGTH_SHORT)
+						.show();
 				finish();
 			}
 		}
