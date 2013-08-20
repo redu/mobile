1aabe6fb86b11e1025881d1eac3bef7c090a12b6
Luís Medeiros <lpmfilho@gmail.com>
2013-08-19 21:17:05 -0300

    Fix: Now it's possible just answer a status of the type answer, help or activity.

--- code/src/br/com/redu/redumobile/activities/StatusDetailActivity.java
+++ code/src/br/com/redu/redumobile/activities/StatusDetailActivity.java
@@ -42,72 +42,75 @@ public class StatusDetailActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState, R.layout.activity_status_detail);
-	
+
 		Bundle extras = getIntent().getExtras();
 		mStatus = (Status) extras.get(EXTRAS_STATUS);
 		boolean showGoToWallAction = extras.getBoolean(EXTRAS_ENABLE_GO_TO_WALL_ACTION, false);
 		boolean isFromNotification = extras.getBoolean(EXTRAS_IS_FROM_NOTIFICATION, false);
 
 		String title = mStatus.getLastBreadcrumb();
-		if(title != null) {
+		if (title != null) {
 			setActionBarTitle(title);
 		}
-		
+
 		if (showGoToWallAction && !mStatus.isPostedOnUserWall()) {
-			addActionToActionBar(R.drawable.bt_go_to_wall,
-					new OnClickListener() {
-						@Override
-						public void onClick(View v) {
-							Intent i = null;
-							if (mStatus.isPostedOnLectureWall()) {
-								i = new Intent(StatusDetailActivity.this, LectureActivity.class);
-								i.putExtra(LectureActivity.EXTRAS_ENVIRONMENT_PATH, mStatus.getEnvironmentPath());
-								i.putExtra(LectureActivity.EXTRAS_SPACE_ID, mStatus.getSpaceId());
-								i.putExtra(LectureActivity.EXTRAS_SUBJECT_ID, mStatus.getSubjectId());
-								i.putExtra(LectureActivity.EXTRAS_LECTURE_ID, mStatus.getLectureId());
+			addActionToActionBar(R.drawable.bt_go_to_wall, new OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Intent i = null;
+					if (mStatus.isPostedOnLectureWall()) {
+						i = new Intent(StatusDetailActivity.this, LectureActivity.class);
+						i.putExtra(LectureActivity.EXTRAS_ENVIRONMENT_PATH, mStatus.getEnvironmentPath());
+						i.putExtra(LectureActivity.EXTRAS_SPACE_ID, mStatus.getSpaceId());
+						i.putExtra(LectureActivity.EXTRAS_SUBJECT_ID, mStatus.getSubjectId());
+						i.putExtra(LectureActivity.EXTRAS_LECTURE_ID, mStatus.getLectureId());
 
-							} else if (mStatus.isPostedOnSpaceWall()) {
-								i = new Intent(StatusDetailActivity.this, SpaceActivity.class);
-								i.putExtra(SpaceActivity.EXTRAS_SPACE_ID, mStatus.getSpaceId());
+					} else if (mStatus.isPostedOnSpaceWall()) {
+						i = new Intent(StatusDetailActivity.this, SpaceActivity.class);
+						i.putExtra(SpaceActivity.EXTRAS_SPACE_ID, mStatus.getSpaceId());
 
-								Bundle extras = new Bundle();
-								extras.putString(SpaceActivity.EXTRAS_ENVIRONMENT_PATH, mStatus.getEnvironmentPath());
-								extras.putString(SpaceActivity.EXTRAS_SPACE_ID, mStatus.getSpaceId());
-								setUpClass(SpaceActivity.class, extras);
+						Bundle extras = new Bundle();
+						extras.putString(SpaceActivity.EXTRAS_ENVIRONMENT_PATH, mStatus.getEnvironmentPath());
+						extras.putString(SpaceActivity.EXTRAS_SPACE_ID, mStatus.getSpaceId());
+						setUpClass(SpaceActivity.class, extras);
 
-							}
+					}
 
-							startActivity(i);
-						}
-					});
+					startActivity(i);
+				}
+			});
 		}
 
-		if(isFromNotification) {
+		if (isFromNotification) {
 			setUpClasses();
 		}
-		
+
 		mInflater = LayoutInflater.from(this);
 
-		mFooterLoadingAnswers = mInflater.inflate(
-				R.layout.status_detail_footer_loading_answers, null);
+		mFooterLoadingAnswers = mInflater.inflate(R.layout.status_detail_footer_loading_answers, null);
 
 		mListView = (ListView) findViewById(R.id.list);
 		mListView.addHeaderView(createStatusHeaderView(mStatus));
 
-		((StatusComposer) findViewById(R.id.status_composer))
-				.setOnStatusComposerListener(new OnStatusComposerListener() {
-					@Override
-					public void onSendClicked(String text) {
-						new PostAnswerTask().execute(mStatus.id, text);
-					}
-				});
+		StatusComposer statusComposer = (StatusComposer) findViewById(R.id.status_composer);
+		if (mStatus.isActivityType() || mStatus.isAnswerType() || mStatus.isHelpType()) {
+			statusComposer.setVisibility(View.VISIBLE);
+			statusComposer.setOnStatusComposerListener(new OnStatusComposerListener() {
+				@Override
+				public void onSendClicked(String text) {
+					new PostAnswerTask().execute(mStatus.id, text);
+				}
+			});
+		} else {
+			statusComposer.setVisibility(View.GONE);
+		}
 
 		mAdapter = new StatusDetailAdapter(getApplicationContext(), null);
 		mListView.setAdapter(mAdapter);
-		
+
 		new LoadAnswersStatus(mStatus.id).execute();
 	}
-	
+
 	private void setUpClasses() {
 		if (mStatus.isPostedOnLectureWall()) {
 			Bundle extrasToUp = new Bundle();
@@ -127,18 +130,14 @@ public class StatusDetailActivity extends BaseActivity {
 	}
 
 	private View createStatusHeaderView(Status status) {
-		View v = mInflater.inflate(
-				R.layout.status_detail_header_original_status, null);
+		View v = mInflater.inflate(R.layout.status_detail_header_original_status, null);
 
 		((Breadcrumb) v.findViewById(R.id.tv_breadcrumb)).setStatus(status);
 
-		((WebCachedImageView) v.findViewById(R.id.iv_photo))
-				.setImageUrl(status.user.getThumbnailUrl());
-		((TextView) v.findViewById(R.id.tv_date)).setText(DateUtil
-				.getFormattedStatusCreatedAt(status));
+		((WebCachedImageView) v.findViewById(R.id.iv_photo)).setImageUrl(status.user.getThumbnailUrl());
+		((TextView) v.findViewById(R.id.tv_date)).setText(DateUtil.getFormattedStatusCreatedAt(status));
 
-		StringBuffer userActionBuffer = new StringBuffer().append("<b>")
-				.append(status.user.getCompleteName()).append("</b>");
+		StringBuffer userActionBuffer = new StringBuffer().append("<b>").append(status.user.getCompleteName()).append("</b>");
 
 		if (status.isActivityType()) {
 			userActionBuffer.append(" comentou");
@@ -153,8 +152,7 @@ public class StatusDetailActivity extends BaseActivity {
 			v.findViewById(R.id.iv_help_icon).setVisibility(View.VISIBLE);
 		}
 
-		((TextView) v.findViewById(R.id.tv_user_action)).setText(Html
-				.fromHtml(userActionBuffer.toString()));
+		((TextView) v.findViewById(R.id.tv_user_action)).setText(Html.fromHtml(userActionBuffer.toString()));
 
 		((TextView) v.findViewById(R.id.tv_text)).setText(status.text);
 
@@ -186,8 +184,7 @@ public class StatusDetailActivity extends BaseActivity {
 	// return v;
 	// }
 
-	class LoadAnswersStatus extends
-			AsyncTask<Void, Void, List<br.com.developer.redu.models.Status>> {
+	class LoadAnswersStatus extends AsyncTask<Void, Void, List<br.com.developer.redu.models.Status>> {
 		private String mStatusId;
 
 		public LoadAnswersStatus(String statusId) {
@@ -201,13 +198,11 @@ public class StatusDetailActivity extends BaseActivity {
 		}
 
 		@Override
-		protected List<br.com.developer.redu.models.Status> doInBackground(
-				Void... params) {
+		protected List<br.com.developer.redu.models.Status> doInBackground(Void... params) {
 			List<br.com.developer.redu.models.Status> answers;
 
 			try {
-				DefaultReduClient redu = ReduApplication
-						.getReduClient(StatusDetailActivity.this);
+				DefaultReduClient redu = ReduApplication.getReduClient(StatusDetailActivity.this);
 				answers = redu.getAnswers(mStatusId);
 			} catch (Exception e) {
 				// TODO show message for internet problems
@@ -219,12 +214,10 @@ public class StatusDetailActivity extends BaseActivity {
 		}
 
 		@Override
-		protected void onPostExecute(
-				List<br.com.developer.redu.models.Status> answers) {
+		protected void onPostExecute(List<br.com.developer.redu.models.Status> answers) {
 			if (answers != null) {
 				if (answers.size() > 0) {
-					DbHelper dbHelper = DbHelper
-							.getInstance(StatusDetailActivity.this);
+					DbHelper dbHelper = DbHelper.getInstance(StatusDetailActivity.this);
 					dbHelper.updateStatusAnswersCount(mStatusId, answers.size());
 
 					mAdapter.addAll(answers);
@@ -232,12 +225,9 @@ public class StatusDetailActivity extends BaseActivity {
 
 					mListView.removeFooterView(mFooterLoadingAnswers);
 				} else {
-					mFooterLoadingAnswers.findViewById(R.id.pb).setVisibility(
-							View.GONE);
-					mFooterLoadingAnswers.findViewById(R.id.tv_loading)
-							.setVisibility(View.GONE);
-					mFooterLoadingAnswers.findViewById(R.id.tv_empty_list)
-							.setVisibility(View.VISIBLE);
+					mFooterLoadingAnswers.findViewById(R.id.pb).setVisibility(View.GONE);
+					mFooterLoadingAnswers.findViewById(R.id.tv_loading).setVisibility(View.GONE);
+					mFooterLoadingAnswers.findViewById(R.id.tv_empty_list).setVisibility(View.VISIBLE);
 				}
 			} else {
 				// TODO handler no internet problem connection
@@ -246,8 +236,7 @@ public class StatusDetailActivity extends BaseActivity {
 		}
 	}
 
-	class PostAnswerTask extends
-			AsyncTask<String, Void, br.com.developer.redu.models.Status> {
+	class PostAnswerTask extends AsyncTask<String, Void, br.com.developer.redu.models.Status> {
 
 		@Override
 		protected void onPreExecute() {
@@ -255,15 +244,13 @@ public class StatusDetailActivity extends BaseActivity {
 		}
 
 		@Override
-		protected br.com.developer.redu.models.Status doInBackground(
-				String... params) {
+		protected br.com.developer.redu.models.Status doInBackground(String... params) {
 			String statusId = params[0];
 			String text = params[1];
 
 			br.com.developer.redu.models.Status status;
 			try {
-				DefaultReduClient redu = ReduApplication
-						.getReduClient(StatusDetailActivity.this);
+				DefaultReduClient redu = ReduApplication.getReduClient(StatusDetailActivity.this);
 				status = redu.postAnswer(statusId, text);
 			} catch (Exception e) {
 				status = null;
@@ -277,14 +264,10 @@ public class StatusDetailActivity extends BaseActivity {
 			dismissProgressDialog();
 
 			if (result == null) {
-				Toast.makeText(StatusDetailActivity.this,
-						"Não foi possível enviar sua resposta.",
-						Toast.LENGTH_SHORT).show();
+				Toast.makeText(StatusDetailActivity.this, "Não foi possível enviar sua resposta.", Toast.LENGTH_SHORT).show();
 			} else {
-				Toast.makeText(StatusDetailActivity.this,
-						"Resposta enviada com sucesso.", Toast.LENGTH_SHORT)
-						.show();
-				
+				Toast.makeText(StatusDetailActivity.this, "Resposta enviada com sucesso.", Toast.LENGTH_SHORT).show();
+
 				mAdapter.add(result);
 				mAdapter.notifyDataSetChanged();
 			}
